e53d19ec833510391085b8bc1fd9fb0b1159dc72
Samy Braik <braiksamy@proton.me>
2024-07-04 12:13:47 +0200

    New API, working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <string>
+#include <sstream>
+#include <vector>
+
 
 CURL* curl; 
 CURLcode res;
@@ -16,13 +19,13 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) 
     return newLength;
 }
 
-void recuperationSpot(const std::string& ticker){
+std::string recuperationSpot(const std::string& ticker, const std::string interval, const std::string key){
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     std::string readBuffer;
 
     if (curl){
-        std::string url = "https://query1.finance.yahoo.com/v8/finance/chart/"+ticker;
+        std::string url = "https://api.twelvedata.com/time_series?symbol="+ticker+"&interval="+interval+"&apikey="+key;
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
@@ -33,28 +36,29 @@ void recuperationSpot(const std::string& ticker){
         curl_easy_cleanup(curl);
     }
 
-    std::cout << "Données reçues : " << readBuffer << std::endl;        
+    return readBuffer;        
 }
 
 
-// void recuperationDonneesHistorique(const std::string& ticker){
-//     curl_global_init(CURL_GLOBAL_DEFAULT);
-//     curl = curl_easy_init();
-//     std::string readBuffer;
-//     if (curl){
-//         curl_easy_setopt(curl, CURLOPT_URL, "https://fr.finance.yahoo.com/quote/"+ticker+"/history/");
-//         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-//         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-//         res = curl_easy_perform(curl);
-//         if(res != CURLE_OK) {
-//             std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-//         }
-//         curl_easy_cleanup(curl);
-//     }
-// }
+std::vector<std::string> split (const std::string &s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss (s);
+    std::string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+
+    return result;
+}
 
 int main(){
+    std::string key = "***";
     std::string ticker = "GOOG";
-    recuperationSpot(ticker);
+    std::string interval = "1day";
+    std::string data = recuperationSpot(ticker,interval,key);
+    std::vector splited = split(data,'{');
+    for (auto i : splited) std::cout << i << std::endl;
+
     return 0;
 }
